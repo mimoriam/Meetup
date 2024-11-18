146f3f7af3b7b1b2d78509ddd57867ba6588e28e
Mimo <mimoriam420@gmail.com>
2024-11-19 03:57:31 +0500

    feat(app): Supabase login with Auth page only

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,21 +1,22 @@
-import { Tabs } from 'expo-router';
+import { Redirect, Tabs } from 'expo-router';
 
 import { TabBarIcon } from '~/components/TabBarIcon';
 
 export default function TabLayout() {
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: 'black',
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Events',
-          headerShown: false,
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+  return <Redirect href="/login" />;
+  // return (
+  //   <Tabs
+  //     screenOptions={{
+  //       tabBarActiveTintColor: 'black',
+  //     }}>
+  //     <Tabs.Screen
+  //       name="index"
+  //       options={{
+  //         title: 'Events',
+  //         headerShown: false,
+  //         tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+  //       }}
+  //     />
+  //   </Tabs>
+  // );
 }
