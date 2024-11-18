b60ee1584e748b9639ee2bd7a91d3715d53bc5d4
Mimo <mimoriam420@gmail.com>
2024-11-18 18:07:09 +0500

    feat(app): event details page

--- components/EventListItem.tsx
+++ components/EventListItem.tsx
@@ -1,5 +1,7 @@
 import Feather from '@expo/vector-icons/Feather';
-import { View, Text, Image } from 'react-native';
+import dayjs from 'dayjs';
+import { Link } from 'expo-router';
+import { View, Text, Image, Pressable } from 'react-native';
 
 export type Event = {
   id: string;
@@ -12,31 +14,33 @@ export type Event = {
 
 export default function EventListItem({ event }: { event: Event }) {
   return (
-    <View className="gap-3 p-3">
-      <View className="flex-row">
-        <View className="flex-1 gap-2">
-          <Text className="text-lg font-semibold uppercase text-amber-800">
-            Wed 13, Sep · 19:30 CET
-          </Text>
+    <Link href={`/${event.id}`} asChild>
+      <Pressable className="m-3 gap-3 border-b-2 border-gray-100 pb-3">
+        <View className="flex-row">
+          <View className="flex-1 gap-2">
+            <Text className="text-lg font-semibold uppercase text-amber-800">
+              {dayjs(event.datetime).format('ddd, D MMM')} ·{' '}
+              {dayjs(event.datetime).format('h:mm A')}
+            </Text>
+            <Text className="text-xl font-bold" numberOfLines={2}>
+              {event.title}
+            </Text>
 
-          <Text className="text-xl font-bold" numberOfLines={2}>
-            {event.title}
-          </Text>
+            <Text className="text-gray-700">{event.location}</Text>
+          </View>
 
-          <Text className="text-gray-700">{event.location}</Text>
+          {/* Event image */}
+          <Image source={{ uri: event.image }} className="aspect-video w-2/5 rounded-xl" />
         </View>
 
-        {/* Event image */}
-        <Image source={{ uri: event.image }} className="aspect-video w-2/5 rounded-xl" />
-      </View>
+        {/* Footer */}
+        <View className="flex-row gap-3">
+          <Text className="mr-auto text-gray-700">16 going</Text>
 
-      {/* Footer */}
-      <View className="flex-row gap-3">
-        <Text className="mr-auto text-gray-700">16 going</Text>
-
-        <Feather name="share" size={20} color="gray" />
-        <Feather name="bookmark" size={20} color="gray" />
-      </View>
-    </View>
+          <Feather name="share" size={20} color="gray" />
+          <Feather name="bookmark" size={20} color="gray" />
+        </View>
+      </Pressable>
+    </Link>
   );
 }
